5ff9fa6678cfeb33f2eff5eb8d1475308606c021
William Walsh <wmdwalsh0@gmail.com>
2021-12-12 16:44:33 -0500

    completed assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,57 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  # GET /spices
+  def index
+    spices = Spice.all
+    render json: spices
+  end
+
+
+  # POST /spices
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
+
+  # GET /spices/:id
+  def show
+    spice = Spice.find_by(id: params[:id])
+    render json: spice
+  end
+
+  # PATCH /spices/:id
+  def update
+    spice = find_spice
+    spice.update(spice_params)
+    render json: spice
+  end
+
+  # PATCH /spices/:id/
+  def is_in_stock
+    spice = find_spice
+    spice.update(spice_params)
+    render json: spice
+  end
+
+  # DELETE /spices/:id
+  def destroy
+    spice = find_spice
+    spice.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_spice
+    Spice.find(params[:id])
+  end
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+
+  def render_not_found_response
+    render json: { error: "Spice not found" }, status: :not_found
+  end
+end
